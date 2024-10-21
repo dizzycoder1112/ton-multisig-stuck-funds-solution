a20b4beed3d14acf568c16533fe8f5a62f4101a9
Dizzycode <champerwu.work@gamil.com>
2024-10-22 04:48:57 +0800

    finish cli app

--- index.ts
+++ index.ts
@@ -1,20 +1,9 @@
 import { derivePath } from 'ed25519-hd-key'
 import {  mnemonicToSeed } from 'bip39'
 import { KeyPair, keyPairFromSeed, mnemonicToPrivateKey, sign } from '@ton/crypto'
-import { Address, beginCell, Builder, Cell, external, internal, SendMode, StateInit, storeMessage, storeMessageRelaxed, toNano, WalletContractV4 } from '@ton/ton'
-import { input } from '@inquirer/prompts';
-
-const to_hex_array: string[] = [];
-const to_byte_map: Record<string, number> = {};
-for (let ord = 0; ord <= 0xff; ord++) {
-  let s = ord.toString(16);
-  if (s.length < 2) {
-    s = "0" + s;
-  }
-  to_hex_array.push(s);
-  to_byte_map[s] = ord;
-}
-
+import { Address, beginCell, Builder, Cell, external, internal, SendMode, StateInit, storeMessage, storeMessageRelaxed, toNano, WalletContractV4, WalletContractV5R1 } from '@ton/ton'
+import { input, password, rawlist } from '@inquirer/prompts';
+import chalk from "chalk";
 
 
 async function extractKeysFromLedgerMnemonic(mnemonic: string, index: number) {
@@ -27,6 +16,19 @@ async function extractKeysFromLedgerMnemonic(mnemonic: string, index: number) {
   })
 
   return { contract: wallet, keyPair }
+}
+
+async function extractKeysFromMyTonWalletMultiChainMnemonic(mnemonic: string, index: number) {
+  const seed = await mnemonicToSeed(mnemonic);
+  const seedContainer = derivePath(`m/44'/607'/${index}'`, seed.toString('hex'));
+  const keyPair = keyPairFromSeed(seedContainer.key);
+  const wallet = WalletContractV5R1.create({
+    workchain: 0,
+    publicKey: keyPair.publicKey,
+  });
+
+  return { contract: wallet, keyPair }
+
 }
 
 class TONX {
@@ -95,7 +97,7 @@ class Wallet {
   rawSign(orderHashHex: string) {
     const signature = sign(Buffer.from(orderHashHex, 'hex'), this.keyPair.secretKey)
     return {
-      signature,
+      buffer: signature,
       signatureHex: signature.toString('hex'),
     }
     // const signature = nacl.sign.detached(hexToBytes(orderHashHex), this.keyPair.secretKey);
@@ -202,31 +204,43 @@ function  createExternalMessageWithSignatures(
   }
   tmpBuilder.storeSlice(orderCell.beginParse());
   const bodyHash = tmpBuilder.endCell().hash().toString('hex');
-  const signature = wallet.rawSign2(bodyHash);
+  const sign = wallet.rawSign(bodyHash);
 
   const bodyBuilder = new Builder();
-  bodyBuilder.storeBuffer(Buffer.from(signature, 'hex'));
+  bodyBuilder.storeBuffer(sign.buffer);
   bodyBuilder.storeSlice(tmpBuilder.endCell().beginParse());
 
   return bodyBuilder.endCell().toBoc()
+}
+
+enum WalletType {
+  NormalV4 = 0,
+  Ledger = 1,
+  MyTonWalletMultiChain = 2,
 }
 
 
 async function main() {
   let tonkeyAddress: Address;
   let thresholds: number;
-  const mnemonics: string[] = [];
+  const ledgerMnemonics: {
+    mnemonic: string,
+    nonBounceableAddress: string,
+    index: number,
+    type: WalletType
+  }[] = [];
+  const wallets: {wallet: Wallet, index: number}[] = [];
 
   try {
-    tonkeyAddress = Address.parse(await input({ message: 'Enter your tonkey address: ' }));
-    console.info(`your tonkey address is ${tonkeyAddress.toString({ bounceable: false })}`);
+    tonkeyAddress = Address.parse(await input({ message: 'Enter your tonkey address:' }));
+    console.info(`your tonkey non-bounceable address is ${tonkeyAddress.toString({ bounceable: false })}`);
   } catch (error) {
     console.error(error);
     throw new Error('Invalid address');
   }
 
   try {
-    thresholds = parseInt(await input({ message: 'Enter the threshold of your multiSig wallet: ' }));
+    thresholds = parseInt(await input({ message: 'Enter the threshold of your multiSig wallet:'}));
     console.log(`your threshold is ${thresholds}`);
   } catch (error) {
     console.error(error);
@@ -234,72 +248,109 @@ async function main() {
   }
 
   for (let i = 0; i < thresholds; i++) {
-    mnemonics.push(await input({ message: `Enter the mnemonic of the ${i + 1}th key` }));
+    const type = await rawlist({ 
+      message: `What's your wallet type?`,
+      choices: [
+        {
+          name: 'normal',
+          value: WalletType.NormalV4
+        },
+        {
+          name: 'ledger',
+          value: WalletType.Ledger
+        },
+        {
+          name: 'MyTonWallet multi chain',
+          value: WalletType.MyTonWalletMultiChain
+        }
+      ] 
+    });
+    const mnemonic = await password({ message: `Enter the mnemonic of the key:`, mask: true});
+    const nonBounceableAddress = await input({ message: `Enter the non-bounceable address of the key:` });
+    const index = await input({ message: `Enter the index of the key:` });
+
+    const result = {
+      mnemonic,
+      nonBounceableAddress,
+      index: parseInt(index),
+      type
+    }
+
+    ledgerMnemonics.push(result);
   }
 
+  for (const m of ledgerMnemonics) {
+    //TODO: make it multi-type
+    let index = 0
+    let found = false
+    switch (m.type) {
+      case WalletType.NormalV4:
+        const keyPair = await mnemonicToPrivateKey(m.mnemonic.split(' '))
+        const wallet = new Wallet(keyPair)
+        wallets.push({wallet, index: m.index})
+        break;
+      case WalletType.Ledger:
+        index = 0
+        found = false
+        while(!found){
+          const { contract, keyPair } = await extractKeysFromLedgerMnemonic(m.mnemonic, index)
+          const address = contract.address.toString({ bounceable: false })
+          // const privateKeyHex = keyPair.secretKey.toString('hex').slice(0, 64)
+          if(address === m.nonBounceableAddress){
+            const publicKeyHex = keyPair.publicKey.toString('hex')
+            console.log(`Found the keypair for the target address ${m.nonBounceableAddress}`)
+            console.log(`Public key: ${publicKeyHex}`)
+            const wallet = new Wallet(keyPair)
+            wallets.push({wallet, index: m.index})
+            found = true
+          }
+          index++
+        }
+        break;
+      case WalletType.MyTonWalletMultiChain:
+        index = 0
+        found = false
+        while(!found){
+          const { contract, keyPair } = await extractKeysFromMyTonWalletMultiChainMnemonic(m.mnemonic, index)
+          const address = contract.address.toString({ bounceable: false })
+          // const privateKeyHex = keyPair.secretKey.toString('hex').slice(0, 64)
+          if(address === m.nonBounceableAddress){
+            const publicKeyHex = keyPair.publicKey.toString('hex')
+            console.log(`Found the keypair for the target address ${m.nonBounceableAddress}`)
+            console.log(`Public key: ${publicKeyHex}`)
+            const wallet = new Wallet(keyPair)
+            wallets.push({wallet, index: m.index})
+            found = true
+          }
+          index++
+        }
+        break;
 
+    }
+  };
 
+  const rawTxhash = await input({ message: 'Enter the raw tx hash:' });
+  const orderCellBoc = await input({ message: 'Enter the order cell boc:' });
+  const signatures: [Uint8Array, number][] = [];
+  wallets.forEach((w, i) => {
+    const sign = w.wallet.rawSign(rawTxhash)
+    signatures.push([sign.buffer, w.index]);
+    if(i === thresholds - 1){
+      const boc = createExternalMessageWithSignatures(w.index, Cell.fromHex(orderCellBoc), signatures, w.wallet)
+      const message = external({
+        to: tonkeyAddress,
+        body: Cell.fromBoc(boc)[0]
+      })
+      const extMsgCell = beginCell()
+      .store(storeMessage(message))
+      .endCell()
+      console.log(chalk.magenta('your external message boc is following above, please copy it:'))
+      
+      console.log(chalk.green(extMsgCell.toBoc().toString('base64')))  
+    }
+  })
 
 
 }
 
 main()
-
-
-// async function run(){
-//   const mnemonic = ""
-//   const wallet = await Wallet.init(mnemonic)
-//   const rawTxhash = "8da63e8b87b37ae43c83787be78b6cb66eb914b3fd487a20fb33ed4ac60a5bc7"
-//   const orderCellBoc = "b5ee9c7241010201004800011e00008e388cae08ac0000000100000301006842000d4657ab40e2a465a4a8b16229e180b483bd4ca12ff56e88288cbc34dbfa4f9f202faf080000000000000000000000000000d7be5224"
-//   const signature  = wallet.rawSign(rawTxhash)
-//   const signatures: [Uint8Array, number][] = [];
-//   signatures.push([signature.signature, 0]);
-
-//   const ledgerMnemonic =""
-//   const targetTonWalletAddress = "UQBUQSw-F6EMKVpun_Uj_raPCczMyU0mw01W2ZRPrpJiBo-J" //it's non-bounceable address
-
-//   let keyPair: KeyPair | null = null;
-//   let index = 0
-//   let found = true
-//   while(found){
-//     const { contract, keyPair: tmpKeyPair } = await extractKeysFromLedgerMnemonic(ledgerMnemonic, index)
-//     const address = contract.address.toString({ bounceable: false })
-//     const publicKeyHex = tmpKeyPair.publicKey.toString('hex')
-//     const privateKeyHex = tmpKeyPair.secretKey.toString('hex').slice(0, 64)
-//     console.log(`new target address ${address}`)
-//     console.log(`Public key: ${publicKeyHex}`)
-//     console.log(`Private key: ${privateKeyHex}`)
-//     if(address === targetTonWalletAddress){
-//       console.log(`Found the keypair for the target address ${targetTonWalletAddress}`)
-//       keyPair = tmpKeyPair
-//       found = false
-//     }
-
-//     index++
-//   }
-//   if(!keyPair){
-//     throw new Error(`Could not find the keypair for the target address ${targetTonWalletAddress}`)
-//   }
-//   const wallet2 = new Wallet(keyPair)
-//   console.log(wallet2)
-//   const sign2 = wallet2.rawSign(rawTxhash)
-//   signatures.push([sign2.signature, 1]);
-  
-
-//   const boc = createExternalMessageWithSignatures(1, Cell.fromHex(orderCellBoc), signatures, wallet2)
-//   // const bocHex = createExternalMessageWithSignatures2(1, cell3FromString(orderCellBoc).stringCell, signatures, wallet2.rawSign2)
-
-//   const a = external({
-//     to: Address.parse('EQA73dVDgA1DyuhDsdSQR6cdpR9H8gM5lrnso-huxy6iBIDZ'),
-//     body: Cell.fromBoc(boc)[0]
-//   })
-//   console.log(a)
-
-//   const extMsgCell = beginCell()
-//       .store(storeMessage(a))
-//       .endCell()
-
-//   console.log(extMsgCell.toBoc().toString('base64'))  
-
-// }
-// run()
